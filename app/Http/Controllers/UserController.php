72c3c86259cb4dc9067064d1831cacfa039e6b2a
waqasijaz <waqasijazx6@gmail.com>
2021-03-11 19:01:50 +0400

    login done

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+
+class UserController extends Controller
+{
+    //
+    function login(Request $req)
+    {
+        $user= user::where(["email"=>$req->email])->first();
+        if(!$user || !Hash::check($req->password, $user->password)){
+            return 'Username and password doesnt match';
+        }
+        else{
+            $req->session()->put('user', $user);
+            return redirect('/');
+        }
+    }
+}
+
